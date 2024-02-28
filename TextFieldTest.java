86cb0d87c0c53596bbba8300cf0ac0bd7e32c4f8
kaiobas <158321614+kaiobas@users.noreply.github.com>
2024-02-28 15:13:05 -0400

    Create TextFieldTest.java

---
+++ TextFieldTest.java
@@ -0,0 +1,21 @@
+
+package javax;
+
+  import javax.TextFieldFrame;
+  import javax.swing.*;
+
+  public class TextFieldTest
+  {
+      public static void main(String[] args) {
+          TextFieldFrame textFieldFrame = new TextFieldFrame();
+          textFieldFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+          textFieldFrame.setSize(350, 100);
+          textFieldFrame.setVisible( true );
+          
+          
+      }
+  
+  
+  
+  }
+
